6ef4839fa75efb568c268a8aeed4c5d76d95fc50
David Kim <hola.hoon@gmail.com>
2020-10-21 10:04:18 +0900

    saving works - working on project page

--- src/components/projectPage/ProjectPage.js
+++ src/components/projectPage/ProjectPage.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-function ProjectPage(props) {
-  const {
-    match: {
-      params: { projectId },
-    },
-  } = props;
+function ProjectPage({ dkData }) {
+  const { projectId } = useParams();
+
+  useEffect(() => {
+    getProjectData(dkData, projectId);
+  }, [dkData, projectId]);
+
+  const getProjectData = (data, projectId) => {
+    data.forEach(({ pageUrl }) => {
+      if (pageUrl === projectId) {
+        console.log(pageUrl);
+      }
+    });
+  };
 
   return (
     <div className='project-page'>
--- src/containers/Works.js
+++ src/containers/Works.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
 
-import useWindowsHeight from "hooks/useWindowsHeight";
 import { dkData } from "components/data/dkData";
+import useWindowsHeight from "hooks/useWindowsHeight";
 import ProjectPage from "components/projectPage/ProjectPage";
 
 function Works({ match }) {
@@ -11,19 +11,18 @@ function Works({ match }) {
   return (
     <div className='works' style={{ height: `${windowInnerHeight}px` }}>
       <section className='works__projects-container'>
-        {dkData.map((project, index) => {
-          const { pageUrl, title, image } = project;
-          return (
-            <div className='project' key={index}>
-              <Link to={`${match.path}/${pageUrl}`}>
-                <img src={image} alt={title} />
-                <span>{title}</span>
-              </Link>
-            </div>
-          );
-        })}
+        {dkData.map(({ pageUrl, title, image }, index) => (
+          <div className='project' key={index}>
+            <Link to={`${match.path}/${pageUrl}`}>
+              <img src={image} alt={title} />
+              <span>{title}</span>
+            </Link>
+          </div>
+        ))}
 
-        <Route path={`${match.path}/:projectId`} component={ProjectPage} />
+        <Route path={`${match.path}/:projectId`}>
+          <ProjectPage dkData={dkData} />
+        </Route>
       </section>
     </div>
   );
