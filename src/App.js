1ba80ba2334b9e8936ae424ae19e86c302e0f826
David Kim <hola.hoon@gmail.com>
2020-11-30 19:46:38 +0900

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import ReactGA from "react-ga";
 
 import Navigation from "containers/Navigation";
 import { LanguageContext } from "hooks/LanguageContext";
@@ -11,6 +12,12 @@ import "./style/main.scss";
 
 function App() {
   const [isLanguageKorean, setIsLanguageKorean] = useState(false);
+
+  useEffect(() => {
+    // Google Analytics
+    ReactGA.initialize("G-ZQE2BZ0R9Z");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <>
